8a508d8ef0698ebdebbed5078eddecb5d0b3e997
Alexander Ortiz <vtacius@gmail.com>
2014-08-26 19:47:00 -0600

    Lectura de Archivo de configuración. Porque debo intentarlo

--- README.md
+++ README.md
@@ -9,5 +9,38 @@ Si ha seguido nuestra guía [Servidor Samba con LDAP NSS – PAM en Debian Wheez
 
     $ aptitude install libauthen-sasl-perl
 
+Cree el fichero __parametros.conf__; copie y personalize el siguiente contenido
+```ini
+
+# Configuración adicional que podemos dejar por defecto
+# Nombre con el que identificamos al servidor dentro del esquema de respaldo.
+# Por defecto, su valor es `hostname`
+ID_SERVER = `hostname`
+
+# Usuario con que el tenemos configuradas las claves publicas para acceder al servidor de respaldo
+usuario = "wwwserver"
+
+# Configuración que mantiene compatibilidad con los demás script y de importancia capital
+servidor_respaldo = "192.168.0.5"
+ruta_respaldo = "/var/respaldo/ldap"
+ruta_respaldo_remoto = "/var/respaldo/${ID_SERVER}/ldap"
+
+# Configuramos las fechas para estampar el nombre de los ficheros.
+# Claro que puede cambiarlas por una que sean más de su gusto,pero no olvide anteponer el signo +
+fecha = "+%d / %m / %Y"
+fecha_archivo = "+%s%m%Y-%H%M"
+
+# Falta implementarlo
+usuario_local = "administrador"
+ruta_log_remoto = "/var/respaldo"
+ruta_log_local = "/var/respaldo"
+correos_notificacion = 'correo@dominio.com.sv'
+
+# Directorio dentro de $ruta_respaldo donde guardaremos los ficheros.
+# En vista a ser lo más configurable posible
+ruta_respaldo_datos = "datos"
+ruta_respaldo_conf = "config"
+```
+
 #### Instrucciones
 Configurelo como una tarea con su gestor de tareas preferidas 
--- conf.pl
+++ conf.pl
@@ -2,9 +2,19 @@
 use strict;
 use warnings;
 use POSIX;
-use Net::LDAP;
-use Net::LDAP::LDIF;
-use Authen::SASL;
+
+sub espacios{
+    my ($palabrejo) = @_;
+    $palabrejo =~ s/^\s|\s$//;
+    $palabrejo =~ s/["|;]$//;
+    return $palabrejo;
+}
+
+sub fechador{
+    my $Config= $_[0];
+    my $clave = $_[1];
+    $Config->{$clave} = strftime($Config->{$clave}, localtime(time));
+}
 
 sub configuracion{
     my ($fichero) = @_;
@@ -12,15 +22,33 @@ sub configuracion{
     open(my $conf, "<", $fichero) or die("Error Abriendo el fichero de configuracion");
     while(<$conf>){
         $config_line=$_;
-        if ( ($config_line !~ /^(#|$)/) ){ 
+        if ( ($config_line !~ /^(#|$)/) ){
             chop($config_line);
-            my @cadena = split (/=/, $config_line, 2); 
-            $item = $cadena[0];        
-            $valor = $cadena[1];
-            $Config{$item} = eval($valor);
+            my @cadena = split (/=/, $config_line, 2);
+            $item = espacios($cadena[0]);
+            $valor = espacios($cadena[1]);
+            if ($valor =~ m/^"\+/){
+                $valor =~ s/\+//;
+                $Config{$item} = strftime($valor, localtime(time));
+            } elsif ($valor =~ m/\$\{.+\}/) {
+                my $string = '<a href="there.html">go here now!</a>';
+                $string =~ m/href="(.*?)"/i;       # extract destination of link
+                print $string;
+                $valor =~ m{\$\{.+\}$}i;
+                print $valor;
+                $Config{$item} = $valor;
+                #print $Config{$item};
+            } else {
+                $Config{$item} = $valor;
+            }
         }
     }
     return %Config;
 }
-my %Config = configuracion("respaldo.conf");
-print %Config;
+
+
+my %Config = configuracion("parametros.conf");
+
+foreach my $item (keys(%Config)){
+    #print $item . ": " .$Config{$item} . "\n"
+}
